bcd4f81f76189f06529e840f5e9965381e2c1dff
gkum4 <gustavokumasawa@gmail.com>
2022-08-30 15:40:40 -0300

    Project and inital settings (eslint, prettier)

---
+++ src/styles/global.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+    font-family: 'Rubik', sans-serif;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    color: #272727;
+  }
+
+  button {
+    border: none;
+    background: none;
+    cursor: pointer;
+  }
+
+  main {
+    background-color: #FBFBFB;
+  }
+`;
